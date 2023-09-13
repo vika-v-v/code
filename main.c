9a145253ccbf73d78a1e03da39aedc9f84367fef
vika-v <vika.vovchenkoo@gmail.com>
2023-09-13 19:48:40 +0200

    to revert

--- main.c
+++ main.c
@@ -7,6 +7,12 @@
 uint8_t hd_mem[4194304];
 uint8_t ra_mem[65536];
 
+struct pageframetabelle_zeile {
+    uint8_t belegt_bit; //0 = nicht belegt; 1 = belegt
+    uint16_t index; //damit ich um den zu einer Seite gehÃ¶rigen Index zu finden nicht durch die ganze Seitentabelle gehen muss
+    uint32_t virt_adress;
+} RAM_pages[16];//hier halte ich fest ob Page-Frames im RAM frei/belegt sind
+
 
 struct seitentabellen_zeile {
 	uint8_t present_bit;
