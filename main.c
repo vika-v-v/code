6011d0f5ec42ca3ca3484a9b5ebe85527e1b0dc9
vika-v <vika.vovchenkoo@gmail.com>
2023-09-13 22:14:18 +0200

    completely changed

--- main.c
+++ main.c
@@ -3,14 +3,8 @@
 #include <stdint.h>
 #include <time.h>
 
-uint8_t hd_mem[4194304];
+uint8_t hd_mem[4194304]; 
 uint8_t ra_mem[65536];
-
-struct pageframetabelle_zeile {
-    uint8_t belegt_bit;
-    uint16_t index;
-    uint32_t virt_adress;
-} RAM_pages[16];
 
 struct seitentabellen_zeile {
     uint8_t present_bit;
@@ -18,17 +12,35 @@ struct seitentabellen_zeile {
     int8_t page_frame;
 } seitentabelle[1024];
 
+int8_t find_empty_frame() {
+    for (int i = 0; i < 16; i++) {
+        int8_t found = 1;
+        for (int j = 0; j < 1024; j++) {
+            if (seitentabelle[j].present_bit && seitentabelle[j].page_frame == i) {
+                found = 0;
+                break;
+            }
+        }
+        if (found) return i;
+    }
+    return -1;
+}
+
 uint16_t get_seiten_nr(uint32_t virt_address) {
     return virt_address >> 12;
 }
 
 uint16_t virt_2_ram_address(uint32_t virt_address) {
     uint16_t ram_address;
-    uint8_t page_frame = seitentabelle[get_seiten_nr(virt_address)].page_frame;
+
+    uint8_t page_frame = seitentabelle[get_seiten_nr(
+            virt_address)].page_frame;
     ram_address = page_frame << 12;
     int offset = virt_address & 4095;
     ram_address = ram_address | offset;
+
     return ram_address;
+
 }
 
 int8_t check_present(uint32_t virt_address) {
@@ -64,12 +76,16 @@ int8_t write_page_to_hd(uint32_t seitennummer, uint32_t virt_address) {
 
 uint16_t swap_page(uint32_t virt_address) {
     int rand_page = rand() % 16;
-    int index = RAM_pages[rand_page].index;
-    int alte_adresse = RAM_pages[rand_page].virt_adress;
 
-    if (seitentabelle[get_seiten_nr(alte_adresse)].dirty_bit == 1) {
-        if (!write_page_to_hd(rand_page, alte_adresse)) {
-            printf("ERROR: failed to write back to HD");
+    for (uint32_t i = 0; i < 1024; i++) {
+        if (seitentabelle[i].present_bit && seitentabelle[i].page_frame == rand_page) {
+            if (seitentabelle[i].dirty_bit == 1) {
+                if (!write_page_to_hd(rand_page, i << 12)) {
+                    printf("ERROR: failed to write back to HD");
+                }
+            }
+            seitentabelle[i].present_bit = 0;
+            break;
         }
     }
 
@@ -81,12 +97,6 @@ uint16_t swap_page(uint32_t virt_address) {
         counter_virt_address++;
     }
 
-    seitentabelle[get_seiten_nr(alte_adresse)].present_bit = 0;
-
-    RAM_pages[rand_page].virt_adress = virt_address;
-    RAM_pages[rand_page].index = get_seiten_nr(virt_address);
-    RAM_pages[rand_page].belegt_bit = 1;
-
     return rand_page;
 }
 
@@ -94,23 +104,15 @@ int8_t get_page_from_hd(uint32_t virt_address) {
     int8_t page_frame;
     int32_t counter_virt_adress = get_seiten_nr(virt_address) << 12;
     
-    if (!mem_is_full()) {
-        for (int seitennummer = 0; seitennummer < 16; seitennummer++) {
-            if (RAM_pages[seitennummer].belegt_bit == 0) {
-                RAM_pages[seitennummer].index = get_seiten_nr(virt_address);
-                RAM_pages[seitennummer].virt_adress = virt_address;
-                RAM_pages[seitennummer].belegt_bit = 1;
+    int8_t empty_frame = find_empty_frame();
+    if (empty_frame != -1) {
+        page_frame = empty_frame;
 
-                page_frame = seitennummer;
-
-                int page_frame_anfang = 4096 * seitennummer;
-                int page_frame_ende = 4096 * (seitennummer + 1);
-                for (int page_frame_byte = page_frame_anfang; page_frame_byte < page_frame_ende; page_frame_byte++) {
-                    ra_mem[page_frame_byte] = hd_mem[counter_virt_adress];
-                    counter_virt_adress++;
-                }
-                break;
-            }
+        int page_frame_anfang = 4096 * page_frame;
+        int page_frame_ende = 4096 * (page_frame + 1);
+        for (int page_frame_byte = page_frame_anfang; page_frame_byte < page_frame_ende; page_frame_byte++) {
+            ra_mem[page_frame_byte] = hd_mem[counter_virt_adress];
+            counter_virt_adress++;
         }
     } else {
         page_frame = swap_page(virt_address);
@@ -118,58 +120,140 @@ int8_t get_page_from_hd(uint32_t virt_address) {
 
     seitentabelle[get_seiten_nr(virt_address)].present_bit = 1;
     seitentabelle[get_seiten_nr(virt_address)].page_frame = page_frame;
-    seitentabelle[get_seiten_nr(virt_address)].dirty_bit = 0;
 
     return 1;
 }
 
-void read_byte(uint32_t virt_address) {
-    if (check_present(virt_address)) {
-        uint16_t ra_address = virt_2_ram_address(virt_address);
-        printf("RA: %d: %d\n", ra_address, ra_mem[ra_address]);
-    } else {
+uint8_t get_data(uint32_t virt_address) {
+    uint8_t read_data;
+    if (check_present(virt_address) == 0) {
         get_page_from_hd(virt_address);
-        uint16_t ra_address = virt_2_ram_address(virt_address);
-        printf("RA: %d: %d\n", ra_address, ra_mem[ra_address]);
     }
+    uint16_t ram_address = virt_2_ram_address(virt_address);
+    read_data = ra_mem[ram_address];
+
+    return read_data;
 }
 
-void write_byte(uint32_t virt_address, uint8_t value) {
+void set_data(uint32_t virt_address, uint8_t value) {
     if (!check_present(virt_address)) {
         get_page_from_hd(virt_address);
     }
-    
-    uint16_t ra_address = virt_2_ram_address(virt_address);
-    ra_mem[ra_address] = value;
+    uint16_t ram_address = virt_2_ram_address(virt_address);
+    ra_mem[ram_address] = value;
+
     seitentabelle[get_seiten_nr(virt_address)].dirty_bit = 1;
 }
 
-void init_system() {
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < 1024; i++) {
-        seitentabelle[i].present_bit = 0;
+
+int main(void) {
+    puts("test driver_");
+    static uint8_t hd_mem_expected[4194304];
+    srand(1);
+    fflush(stdout);
+    for (int i = 0; i < 4194304; i++) {
+        uint8_t val = (uint8_t) rand();
+        hd_mem[i] = val;
+        hd_mem_expected[i] = val;
+    }
+
+    for (uint32_t i = 0; i < 1024; i++) {
         seitentabelle[i].dirty_bit = 0;
         seitentabelle[i].page_frame = -1;
-    }
-    for (int i = 0; i < 16; i++) {
-        RAM_pages[i].belegt_bit = 0;
-        RAM_pages[i].index = -1;
-        RAM_pages[i].virt_adress = -1;
-    }
-}
-
-int main() {
-    init_system();
-
-    for (int i = 0; i < 20; i++) {
-        uint32_t addr = rand() % 4194304;
-        write_byte(addr, i);
+        seitentabelle[i].present_bit = 0;
     }
 
-    for (int i = 0; i < 20; i++) {
-        uint32_t addr = rand() % 4194304;
-        read_byte(addr);
+    uint32_t zufallsadresse = 4192425;
+
+    uint8_t value = get_data(zufallsadresse);
+//	printf("value: %d\n", value);
+
+    if (hd_mem[zufallsadresse] != value) {
+        printf("ERROR_ at Address %d, Value %d =! %d!\n", zufallsadresse, hd_mem[zufallsadresse], value);
     }
-    
-    return 0;
+
+    value = get_data(zufallsadresse);
+
+    if (hd_mem[zufallsadresse] != value) {
+        printf("ERROR_ at Address %d, Value %d =! %d!\n", zufallsadresse, hd_mem[zufallsadresse], value);
+
+    }
+
+//		printf("Address %d, Value %d =! %d!\n",zufallsadresse, hd_mem[zufallsadresse], value);
+
+
+    srand(3);
+
+    for (uint32_t i = 0; i <= 1000; i++) {
+        uint32_t zufallsadresse = rand() % 4194304;//i * 4095 + 1;//rand() % 4194303
+        uint8_t value = get_data(zufallsadresse);
+        if (hd_mem[zufallsadresse] != value) {
+            printf("ERROR_ at Address %d, Value %d =! %d!\n", zufallsadresse, hd_mem[zufallsadresse], value);
+            for (uint32_t i = 0; i <= 1023; i++) {
+                //printf("%d,%d-",i,seitentabelle[i].present_bit);
+                if (seitentabelle[i].present_bit) {
+                    printf("i: %d, seitentabelle[i].page_frame %d\n", i, seitentabelle[i].page_frame);
+                    fflush(stdout);
+                }
+            }
+            exit(1);
+        }
+//		printf("i: %d data @ %u: %d hd value: %d\n",i,zufallsadresse, value, hd_mem[zufallsadresse]);
+        fflush(stdout);
+    }
+
+    srand(3);
+
+    for (uint32_t i = 0; i <= 100; i++) {
+        uint32_t zufallsadresse = rand() % 4095 * 7;
+        uint8_t value = (uint8_t) zufallsadresse >> 1;
+        set_data(zufallsadresse, value);
+        hd_mem_expected[zufallsadresse] = value;
+//		printf("i : %d set_data address: %d - %d value at ram: %d\n",i,zufallsadresse,(uint8_t)value, ra_mem[virt_2_ram_address(zufallsadresse)]);
+    }
+
+
+    srand(4);
+    for (uint32_t i = 0; i <= 16; i++) {
+        uint32_t zufallsadresse = rand() % 4194304;//i * 4095 + 1;//rand() % 4194303
+        uint8_t value = get_data(zufallsadresse);
+        if (hd_mem_expected[zufallsadresse] != value) {
+//			printf("ERROR_ at Address %d, Value %d =! %d!\n",zufallsadresse, hd_mem[zufallsadresse], value);
+            for (uint32_t i = 0; i <= 1023; i++) {
+                //printf("%d,%d-",i,seitentabelle[i].present_bit);
+                if (seitentabelle[i].present_bit) {
+                    printf("i: %d, seitentabelle[i].page_frame %d\n", i, seitentabelle[i].page_frame);
+                    fflush(stdout);
+                }
+            }
+
+            exit(2);
+        }
+//		printf("i: %d data @ %u: %d hd value: %d\n",i,zufallsadresse, value, hd_mem[zufallsadresse]);
+        fflush(stdout);
+    }
+
+    srand(3);
+    for (uint32_t i = 0; i <= 2500; i++) {
+        uint32_t zufallsadresse = rand() % (4095 * 5);//i * 4095 + 1;//rand() % 4194303
+        uint8_t value = get_data(zufallsadresse);
+        if (hd_mem_expected[zufallsadresse] != value) {
+            printf("%d\n", i);
+            printf("ERROR_ at Address %d, Value %d =! %d!\n", zufallsadresse, hd_mem_expected[zufallsadresse], value);
+            for (uint32_t i = 0; i <= 1023; i++) {
+                //printf("%d,%d-",i,seitentabelle[i].present_bit);
+                if (seitentabelle[i].present_bit) {
+                    printf("i: %d, seitentabelle[i].page_frame %d\n", i, seitentabelle[i].page_frame);
+                    fflush(stdout);
+                }
+            }
+            exit(3);
+        }
+//		printf("i: %d data @ %u: %d hd value: %d\n",i,zufallsadresse, value, hd_mem_expected[zufallsadresse]);
+        fflush(stdout);
+    }
+
+    puts("test end");
+    fflush(stdout);
+    return EXIT_SUCCESS;
 }
